664319c8ed4dcbd62b295efbf4a38f1776d22f59
snorm <snorm@MSI>
2024-11-15 21:47:20 -0600

    SEEMS to be working for 5 cities now.
    Next, make a text file of 10 or so city names and their latitudes and longitudes.
    
    When vertices (cities) are inserted, automatically generate a fully connected graph with distances/weights calculated based on coordinates.
    
    THEN, get into greedy TSP.

--- graphStuff-July2024/TSP-Nov15.cpp
+++ graphStuff-July2024/TSP-Nov15.cpp
@@ -24,6 +24,37 @@ WeightedUndirectedGraph generateGraphOfFourCities()
 
 }
 
+WeightedUndirectedGraph generateGraphOf5Cities()
+{
+	WeightedUndirectedGraph graphOf5Cities;
+
+	//it would be reasonable to add these vertices in ALPHABETICAL order (because next_permutation may be used in TSP algo) 
+	graphOf5Cities.addVertex("Albuquerque");
+	graphOf5Cities.addVertex("Atlanta");
+	graphOf5Cities.addVertex("Austin");
+	graphOf5Cities.addVertex("Chicago");
+	graphOf5Cities.addVertex("Denver");
+
+	graphOf5Cities.addEdge("Albuquerque", "Atlanta", 1250);
+	graphOf5Cities.addEdge("Albuquerque", "Austin", 600);
+	graphOf5Cities.addEdge("Albuquerque", "Chicago", 1100);
+	graphOf5Cities.addEdge("Albuquerque", "Denver", 300);
+
+
+	graphOf5Cities.addEdge("Atlanta", "Austin", 800);
+	graphOf5Cities.addEdge("Atlanta", "Chicago", 550);
+	graphOf5Cities.addEdge("Atlanta", "Denver", 1200);
+
+	graphOf5Cities.addEdge("Austin", "Chicago", 1000);
+	graphOf5Cities.addEdge("Austin", "Denver", 750);
+
+	graphOf5Cities.addEdge("Chicago", "Denver", 900);
+
+
+	return graphOf5Cities;
+
+}
+
 
 int main(int argc, char* argv)
 {
@@ -33,10 +64,18 @@ int main(int argc, char* argv)
 	
 	//srand(time(0));
 
-	graphOfFourCities.bruteForceTSP_November14(startCity); 
+	//graphOfFourCities.bruteForceTSP_November14(startCity); 
 
 
+	auto graphOf5Cities = generateGraphOf5Cities(); 
 
+	//cout << std::boolalpha; 
+	//cout << graphOf5Cities.isCompleteGraph() << "\n";
+
+	graphOf5Cities.bruteForceTSP_November14(startCity); 
+
+	
+	
 	//string endCity = "Chicago"; 
 
 	//graphOfFourCities.takeRandomWalk_UntilSomeEnd(startCity, endCity);
